da027413802dda69d8c0e7068d04c2d3fde3dc51
Zoksss <zorantodorovic80@gmail.com>
2023-07-11 23:01:17 +0200

    transitions, ui fixes

--- src/Components/HomeScreen/HomeScreen.jsx
+++ src/Components/HomeScreen/HomeScreen.jsx
@@ -14,6 +14,8 @@ import Gem from "../../Icons/gem.png"
 import ShopTheme from './ShopTheme';
 import InvTheme from './InvTheme';
 
+import { useTransition, animated } from "react-spring"
+
 const HomeScreen = (props) => {
 
     const [isShop, setIsShop] = React.useState(false);
@@ -24,10 +26,25 @@ const HomeScreen = (props) => {
     const styleNav = { backgroundImage: `url("${props.currentTheme.navSRC}")` }
     const styleBtnWide = { backgroundImage: `url("${props.currentTheme.btnWideSRC}")` }
     const styleCenter = { backgroundImage: `url("${props.currentTheme.centerSRC}")` }
- 
+
+    const transitionMain = useTransition(!isInv && !isShop, {
+        from: {  opacity: 0 },
+        enter: { opacity: 1 },
+        leave: {  opacity: 0 },
+    });
+    const transitionShop = useTransition(!isInv && isShop, {
+        from: { x: 150, y: 0, opacity: 0 },
+        enter: { x: 0, y: 0, opacity: 1 },
+        leave: { x: 150, y: 0, opacity: 0 },
+    });
+    const transitionInv = useTransition(isInv && !isShop, {
+        from: { x: -150, y: 0, opacity: 0 },
+        enter: { x: 0, y: 0, opacity: 1 },
+        leave: { x: -150, y: 0, opacity: 0 },
+    });
 
     return (
-        <div className="home-screen-container" style={styleBack}>
+        <div className="home-screen-container" style={styleBack} >
             <div className="home-screen-top">
                 <div className="nickname" style={styleTop}>
                     <p>Zoks</p>
@@ -44,53 +61,58 @@ const HomeScreen = (props) => {
                 </div>
             </div>
 
-            {isShop && !isInv && <div className="shop-container" >
-                <button className="center-shape-image shop-promo" style={styleCenter}>
-                    <p className="shop-promo-title">ALPHA THEME</p>
-                    <div className="promo-reward-row">
-                        <img className="promo-theme-img" src={"themes/ShopIcons/shop_theme_alpha.png"} alt="" />
-                        <div className="promo-reward-side">
-                            <div><img className="promo-coin-img" src={Coin} alt="" />75</div>
-                            <div><img className="promo-gem-img" src={Gem} alt="" />10</div>
+            {transitionShop((style, item) =>
+                item ? <animated.div style={style} className="shop-container" >
+                    <button className="center-shape-image shop-promo" style={styleCenter}>
+                        <p className="shop-promo-title">ALPHA THEME</p>
+                        <div className="promo-reward-row">
+                            <img className="promo-theme-img" src={"themes/ShopIcons/shop_theme_alpha.png"} alt="" />
+                            <div className="promo-reward-side">
+                                <div><img className="promo-coin-img" src={Coin} alt="" />75</div>
+                                <div><img className="promo-gem-img" src={Gem} alt="" />10</div>
+                            </div>
                         </div>
+                        <p className="shop-promo-desc">CLAIM FOR FREE!</p>
+                    </button>
+                    <div className="theme-shop-contianer">
+                        <ShopTheme price={props.themesData.find(o => o.themeName === "SciFi").price} img={props.themesData.find(o => o.themeName === "SciFi").placeholder} namee={"SciFi"} buyTheme={props.buyTheme} isCheckmark={(props.unlockedThemes.filter(themeName => themeName === "SciFi")).length ? true : false} />
+                        <ShopTheme price={props.themesData.find(o => o.themeName === "Matrix").price} img={props.themesData.find(o => o.themeName === "Matrix").placeholder} namee={"Matrix"} buyTheme={props.buyTheme} isCheckmark={props.unlockedThemes.filter(themeName => themeName === "Matrix").length ? true : false} />
+                        <ShopTheme price={props.themesData.find(o => o.themeName === "Desert").price} img={props.themesData.find(o => o.themeName === "Desert").placeholder} namee={"Desert"} gems={true} buyTheme={props.buyTheme} isCheckmark={props.unlockedThemes.filter(themeName => themeName === "Desert").length ? true : false} />
+                        <ShopTheme price={props.themesData.find(o => o.themeName === "Japan").price} img={props.themesData.find(o => o.themeName === "Desert").placeholder} namee={"Japan"} buyTheme={props.buyTheme} isCheckmark={props.unlockedThemes.filter(themeName => themeName === "Japan").length ? true : false} />
                     </div>
-                    <p className="shop-promo-desc">CLAIM FOR FREE!</p>
-                </button>
-                <div className="theme-shop-contianer">
-                    <ShopTheme price={props.themesData.find(o=>o.themeName==="SciFi").price} img={props.themesData.find(o=>o.themeName==="SciFi").placeholder} namee={"SciFi"} buyTheme={props.buyTheme} isCheckmark={(props.unlockedThemes.filter(themeName => themeName === "SciFi")).length?true:false}/>
-                    <ShopTheme price={props.themesData.find(o=>o.themeName==="Matrix").price} img={props.themesData.find(o=>o.themeName==="Matrix").placeholder} namee={"Matrix"} buyTheme={props.buyTheme} isCheckmark={props.unlockedThemes.filter(themeName => themeName === "Matrix").length?true:false}/>
-                    <ShopTheme price={props.themesData.find(o=>o.themeName==="Desert").price} img={props.themesData.find(o=>o.themeName==="Desert").placeholder} namee={"Desert"} gems={true} buyTheme={props.buyTheme} isCheckmark={props.unlockedThemes.filter(themeName => themeName === "Desert").length?true:false}/>
-                    <ShopTheme price={props.themesData.find(o=>o.themeName==="Japan").price} img={props.themesData.find(o=>o.themeName==="Desert").placeholder} namee={"Japan"} buyTheme={props.buyTheme} isCheckmark={props.unlockedThemes.filter(themeName => themeName === "Japan").length?true:false}/>
-                </div>
-            </div>}
+                </animated.div> : ""
+            )}
 
-            {isInv && !isShop && <div className="inv-container">
-                <p className="inv-promo-desc">THEME INVENTROY ({props.unlockedThemes.length}/34)</p>
-                <div className="theme-inv-contianer">
-                   {props.unlockedThemes.map((themeName, i) => (
-            <InvTheme img={props.themesData.find(o=>o.themeName===themeName).placeholder} themeName = {themeName} key={i} equipTheme={props.buyTheme} />))}
-                </div>
-            </div>}
+            {transitionInv((style, item) =>
+                item ? <animated.div style={style} className="inv-container">
+                    <p className="inv-promo-desc">THEME INVENTROY ({props.unlockedThemes.length}/34)</p>
+                    <div className="theme-inv-contianer">
+                        {props.unlockedThemes.map((themeName, i) => (
+                            <InvTheme img={props.themesData.find(o => o.themeName === themeName).placeholder} themeName={themeName} key={i} equipTheme={props.buyTheme} />))}
+                    </div>
+                </animated.div> : ""
+            )}
 
 
-            {!isShop && !isInv && <>
+            {transitionMain((style, item) =>
+                item ? <animated.div className="home-wrapper" style={style}>
 
-                <button className="center-shape-image" onClick={() => props.startGame()} style={styleCenter}>
-                    <img src={Triangle} alt="circle" />
-                    <p>PLAY</p>
-                </button>
-                <button className="vs-button" style={styleBtnWide}>
-                    <img src={Pentagon} alt="circle" />
-                    <p>VS</p>
-                </button>
-            </>
-            }
+                    <button className="center-shape-image" onClick={() => props.startGame()} style={styleCenter}>
+                        <img src={Triangle} alt="circle" />
+                        <p>PLAY</p>
+                    </button>
+                    <button className="vs-button" style={styleBtnWide}>
+                        <img src={Pentagon} alt="circle" />
+                        <p>VS</p>
+                    </button>
+                </animated.div> : ""
+            )}
             <div className="home-screen-bottom" style={styleNav}>
                 <button onClick={() => { setIsShop(true); setIsInv(false) }} className={`home-shop-button ${isShop ? "active" : ""}`}><img src={Shop} alt="shop" /></button>
                 <button onClick={() => { setIsShop(false); setIsInv(false); }} className={`home-play-button ${!isShop && !isInv ? "active" : ""}`}><img src={Main} alt="shop" /></button>
                 <button onClick={() => { setIsShop(false); setIsInv(true) }} className={`home-inventory-button ${isInv ? "active" : ""}`}><img src={Inventory} alt="shop" /></button>
             </div>
-        </div>
+        </div >
     );
 }
 
